a7e0c1bf9a14b739c22ee0b6ed04b96de58ef0be
JOSH ROOD <jrood@nhaschools.com>
2022-06-19 13:01:19 -0400

    Inital workspace setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'jasonrood';
+}
